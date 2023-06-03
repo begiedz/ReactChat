6848dfb191f2bd0e7be366bbcd472d1799eec762
Dariusz Begiedza <4207958@teb-gdansk.ad>
2023-06-03 11:48:35 +0200

    add messages styles

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 const Message = () => {
-  return (
-    <div>Message</div>
-  )
-}
+    return (
+        <div className="message">
+            <div className="messageInfo">
+                <img
+                    src="https://cdn.pixabay.com/photo/2023/05/11/03/34/white-cockatoo-7985434_960_720.jpg"
+                    alt="woman"
+                />
+                <span>just now</span>
+            </div>
+            <div className="messageContent">
+                <p>Test</p>
+                <img
+                    src="https://cdn.pixabay.com/photo/2023/05/11/03/34/white-cockatoo-7985434_960_720.jpg"
+                    alt="woman"
+                />
+            </div>
+        </div>
+    );
+};
 
-export default Message
+export default Message;
