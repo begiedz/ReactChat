7025612bb3779d1926e47a819be8077e5bf1361d
Dariusz Begiedza <4207958@teb-gdansk.ad>
2023-06-04 11:41:27 +0200

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './style.scss';
 import Register from './pages/Register';
 import Login from './pages/Login';
-import Home from './pages/Home';
+import Home from './pages/Home';\
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 
 function App() {
     return (
         <div className="App">
-            <Register />
+            <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home/>}/>
+          </Route>
+          <Route path="login" element={<Login/>}/>
+          <Route path="register" element={<Register/>}/>
+        </Routes>
+      </BrowserRouter>
         </div>
     );
 }
