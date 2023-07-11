eb005ded977fca12955f1e65b072de0a4d4a39c3
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-07-11 12:11:09 +0200

    add chat service

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,38 +1,38 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { db } from '../firebase';
+import { doc, onSnapshot } from 'firebase/firestore';
+import { AuthContext } from '../context/AuthContext';
 
 const Chats = () => {
+    const [chats, setChats] = useState([]);
+
+    const { currentUser } = useContext(AuthContext);
+
+    useEffect(() => {
+        const getChats = () => {
+            const unsub = onSnapshot(doc(db, 'userChats', currentUser.uid), (doc) => {
+                console.log(doc);
+                setChats(doc.data());
+            });
+            return () => {
+                unsub();
+            };
+        };
+        currentUser.uid && getChats();
+        console.log(Object.entries(chats));
+    }, [currentUser.uid]);
+
     return (
         <div className="chats">
-            <div className="userChat">
-                <img
-                    src="https://cdn.pixabay.com/photo/2016/12/04/02/01/celtic-woman-1880944__340.jpg"
-                    alt="woman"
-                />
-                <div className="userChatInfo">
-                    <span>Jane</span>
-                    <p>Hello</p>
+            {Object.entries(chats)?.map((chat) => (
+                <div className="userChat" key={chat[0]}>
+                    <img src={chat[1].userInfo.photoURL} alt="" />
+                    <div className="userChatInfo">
+                        <span>{chat[1].userInfo.displayName}</span>
+                        <p>{chat[1].lastMessage?.text}</p>
+                    </div>
                 </div>
-            </div>
-            <div className="userChat">
-                <img
-                    src="https://cdn.pixabay.com/photo/2016/12/04/02/01/celtic-woman-1880944__340.jpg"
-                    alt="woman"
-                />
-                <div className="userChatInfo">
-                    <span>Jane</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-            <div className="userChat">
-                <img
-                    src="https://cdn.pixabay.com/photo/2016/12/04/02/01/celtic-woman-1880944__340.jpg"
-                    alt="woman"
-                />
-                <div className="userChatInfo">
-                    <span>Jane</span>
-                    <p>Hello</p>
-                </div>
-            </div>
+            ))}
         </div>
     );
 };
